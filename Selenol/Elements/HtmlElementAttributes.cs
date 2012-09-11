5289878879c886d40e05596911dc65bb5f9fd55e
mezm <bartleby.sev@gmail.com>
2012-09-11 10:14:44 +0300

    SE-2 Implement textbox element
    separated attribute names into constant class

---
+++ Selenol/Elements/HtmlElementAttributes.cs
@@ -0,0 +1,17 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+namespace Selenol.Elements
+{
+    /// <summary>The html element attribute names.</summary>
+    public static class HtmlElementAttributes
+    {
+        /// <summary>The id attribute name.</summary>
+        public const string Id = "id";
+
+        /// <summary>The name attribute name.</summary>
+        public const string Name = "name";
+
+        /// <summary>The class attribute name.</summary>
+        public const string Class = "class";
+    }
+}
