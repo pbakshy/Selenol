b862ac19578d7c7c2d99ae7dcd47683bfb6a958f
mezm <bartleby.sev@gmail.com>
2012-09-17 16:17:45 +0300

    SE-32 Implement declarative element verification
    added not input attribute

---
+++ Selenol.Tests/Validation/ElementForTest.cs
@@ -0,0 +1,17 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+
+using Selenol.Elements;
+
+namespace Selenol.Tests.Validation
+{
+    [IgnoreValidationForTest]
+    public class ElementForTest : BaseHtmlElement
+    {
+        public ElementForTest(IWebElement webElement)
+            : base(webElement)
+        {
+        } 
+    }
+}
---
+++ Selenol/Validation/NotInputAttribute.cs
@@ -0,0 +1,43 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+using Selenol.Elements;
+
+using Selenol.Extensions;
+
+namespace Selenol.Validation
+{
+    /// <summary>The attribute that validates an element is not an input element.</summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class NotInputAttribute : Attribute, IElementValidator
+    {
+        /// <summary>Validates an element.</summary>
+        /// <param name="element">The element. </param>
+        /// <returns>True if element is valid otherwise false. </returns>
+        public bool Validate(BaseHtmlElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("element");
+            }
+
+            var tagName = element.TagName;
+            return !Equals(tagName, HtmlElements.Input) && !Equals(tagName, HtmlElements.Button) && !Equals(tagName, HtmlElements.Option)
+                   && !Equals(tagName, HtmlElements.Select) && !Equals(tagName, HtmlElements.TextArea);
+        }
+
+        /// <summary>Gets an error message for an invalid element.</summary>
+        /// <param name="element">The invalid element. </param>
+        /// <returns>The error message. </returns>
+        public string GetErrorMessage(BaseHtmlElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("element");
+            }
+
+            return "Expected tag not in set of input, textarea, select, button, option. But was '{0}'.".F(element.TagName);
+        }
+    }
+}
