c40084ae0df3c03cfe657b6a06f171e45f2c73eb
Pavel Bakshy <pavelbakshy@gmail.com>
2012-09-22 14:53:32 +0300

    Moved Tests for Element Validation to Selenol.Tests.Validation.Element namespace

---
+++ Selenol.Tests/Validation/Element/IgnoreValidationForTestAttribute.cs
@@ -0,0 +1,22 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+using Selenol.Elements;
+using Selenol.Validation.Element;
+
+namespace Selenol.Tests.Validation.Element
+{
+    public class IgnoreValidationForTestAttribute : Attribute, IElementValidator
+    {
+        public bool Validate(BaseHtmlElement element)
+        {
+            return true;
+        }
+
+        public string GetErrorMessage(BaseHtmlElement element)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Selenol.Tests/Validation/Element/TestNotInputAttribute.cs
@@ -0,0 +1,59 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+
+using Rhino.Mocks;
+
+using Selenol.Validation.Element;
+
+namespace Selenol.Tests.Validation.Element
+{
+    [TestFixture]
+    public class TestNotInputAttribute
+    {
+        [Test] 
+        [TestCase("div")]
+        [TestCase("p")]
+        [TestCase("span")]
+        public void ValidateCorrectElement(string tagName)
+        {
+            var webElement = MockRepository.GenerateStub<IWebElement>();
+            webElement.Stub(x => x.TagName).Return(tagName);
+
+            var element = new ElementForTest(webElement);
+            var attribute = new NotInputAttribute();
+            attribute.Validate(element).Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase("input")]
+        [TestCase("textarea")]
+        [TestCase("select")]
+        [TestCase("button")]
+        [TestCase("option")]
+        public void ValidateInCorrectElement(string tagName)
+        {
+            var webElement = MockRepository.GenerateStub<IWebElement>();
+            webElement.Stub(x => x.TagName).Return(tagName);
+
+            var element = new ElementForTest(webElement);
+            var attribute = new NotInputAttribute();
+            attribute.Validate(element).Should().BeFalse();
+        }
+
+        [Test]
+        public void GetErrorMessage()
+        {
+            var webElement = MockRepository.GenerateStub<IWebElement>();
+            webElement.Stub(x => x.TagName).Return("input");
+
+            var element = new ElementForTest(webElement);
+            var attribute = new NotInputAttribute();
+            attribute.GetErrorMessage(element).Should().Be("Expected tag not in set of input, textarea, select, button, option. But was 'input'.");
+        }
+    }
+}
