11e65b9c6aa8cf62e30855cac122ada7571673b9
mezm <bartleby.sev@gmail.com>
2012-09-29 22:37:18 +0300

    SE-41 Create functional tests
    covered radio button element

---
+++ Selenol.FunctionalTests/Features/RadioButtonFeature.feature
@@ -0,0 +1,27 @@
+﻿Feature: RadioButtonFeature
+	As an user I want to have ability to work with strong typed radio button element.
+
+Scenario Outline: Get selected value
+	Given that I am viewing "Elements" page
+	Then radio button with id "<id>" has value "<value>"
+
+	Examples: 
+	| id      | value |
+	| radio-1 | false |
+	| radio-2 | true  |
+	| radio-3 | false |
+	
+Scenario: All radio buttons
+	Given that I am viewing "Elements" page
+	Then there are radio buttons with id "radio-1, radio-2, radio-3"
+
+Scenario Outline: Select an option
+	Given that I am viewing "Elements" page
+	When I select radio button with "<id>"
+	Then radio button with id "<id>" has value "true"
+
+	Examples: 
+	| id      |
+	| radio-1 |
+	| radio-2 |
+	| radio-3 |
---
+++ Selenol.FunctionalTests/Steps/RadioButtonSteps.cs
@@ -0,0 +1,41 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+
+using FluentAssertions;
+
+using OpenQA.Selenium;
+
+using Selenol.Elements;
+
+using TechTalk.SpecFlow;
+
+namespace Selenol.FunctionalTests.Steps
+{
+    [Binding]
+    public class RadioButtonSteps
+    {
+        [When(@"I select radio button with ""(.*)""")]
+        public void WhenISelectRadionButtonWith(string id)
+        {
+            GetRadioButton(id).Check();
+        }
+
+        [Then(@"radio button with id ""(.*)"" has value ""(.*)""")]
+        public void ThenRadioButtonWithIdHasValue(string id, bool value)
+        {
+            GetRadioButton(id).IsChecked.Should().Be(value);
+        }
+
+        [Then(@"there are radio buttons with id ""(.*)""")]
+        public void ThenThereAreRadioButtonsWithId(string[] ids)
+        {
+            Browser.Current.RadioButtons().Select(x => x.Id).Should().BeEquivalentTo(ids.AsEnumerable());
+        }
+
+        private static RadioButtonElement GetRadioButton(string id)
+        {
+            return Browser.Current.RadioButton(By.Id(id));
+        }
+    }
+}
