d6e8eb73d64f53a767195641291ab5927ce5ace1
mezm <bartleby.sev@gmail.com>
2012-09-29 22:07:51 +0300

    SE-41 Create functional tests
    covered checkbox element

---
+++ Selenol.FunctionalTests/Features/CheckboxFeature.feature
@@ -0,0 +1,27 @@
+﻿Feature: CheckboxFeature
+	As an user I want to have ability to work with strong typed checkbox element.
+
+Scenario Outline: Get value
+	Given that I am viewing "Elements" page
+	Then checkbox with "<id>" has value "<value>"
+
+	Examples: 
+	| id         | value |
+	| checkbox-1 | true  |
+	| checkbox-2 | false |
+
+Scenario: All checkboxes
+	Given that I am viewing "Elements" page
+	Then there are checkboxes with id "checkbox-1, checkbox-2"
+
+Scenario Outline: Change state
+	Given that I am viewing "Elements" page
+	When I checked checkbox with id "<id>"
+	Then checkbox with "<id>" has value "true"
+	When I uncheck checkbox with id "<id>"
+	Then checkbox with "<id>" has value "false"
+
+	Examples: 
+	| id         |
+	| checkbox-1 |
+	| checkbox-2 |
---
+++ Selenol.FunctionalTests/Steps/CheckboxSteps.cs
@@ -0,0 +1,47 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+
+using FluentAssertions;
+
+using OpenQA.Selenium;
+
+using Selenol.Elements;
+
+using TechTalk.SpecFlow;
+
+namespace Selenol.FunctionalTests.Steps
+{
+    [Binding]
+    public class CheckboxSteps
+    {
+        [When(@"I checked checkbox with id ""(.*)""")]
+        public void WhenICheckedCheckboxWithId(string id)
+        {
+            GetCheckbox(id).Check();
+        }
+
+        [When(@"I uncheck checkbox with id ""(.*)""")]
+        public void WhenIUncheckCheckboxWithId(string id)
+        {
+            GetCheckbox(id).Uncheck();
+        }
+
+        [Then(@"checkbox with ""(.*)"" has value ""(.*)""")]
+        public void ThenChecboxWithHasValue(string id, bool value)
+        {
+            GetCheckbox(id).IsChecked.Should().Be(value);
+        }
+
+        [Then(@"there are checkboxes with id ""(.*)""")]
+        public void ThenThereAreCheckboxesWithId(string[] ids)
+        {
+            Browser.Current.Checkboxes().Select(x => x.Id).Should().BeEquivalentTo(ids.AsEnumerable());
+        }
+ 
+        private static CheckboxElement GetCheckbox(string id)
+        {
+            return Browser.Current.Checkbox(By.Id(id));
+        }
+    }
+}
