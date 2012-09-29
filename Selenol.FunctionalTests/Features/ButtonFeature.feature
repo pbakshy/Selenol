ed869b575a8699801c72322307789d1657728da1
mezm <bartleby.sev@gmail.com>
2012-09-29 22:56:31 +0300

    SE-41 Create functional tests
    covered button element

---
+++ Selenol.FunctionalTests/Features/ButtonFeature.feature
@@ -0,0 +1,16 @@
+﻿Feature: ButtonFeature
+	As an user I want to have ability to work with strong typed button element.
+
+Scenario: All buttons
+	Given that I am viewing "Elements" page
+	Then there are buttons with id "button-1, button-2"
+
+Scenario Outline: Click on button
+	Given that I am viewing "Elements" page
+	When I click on button with "<id>"
+	Then I see an alert with text "<text>"
+
+	Examples: 
+	| id       | text             |
+	| button-1 | Button 1 clicked |
+	| button-2 | Button 2 clicked |
---
+++ Selenol.FunctionalTests/Steps/ButtonSteps.cs
@@ -0,0 +1,39 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+
+using FluentAssertions;
+
+using OpenQA.Selenium;
+
+using Selenol.Elements;
+
+using TechTalk.SpecFlow;
+
+namespace Selenol.FunctionalTests.Steps
+{
+    [Binding]
+    public class ButtonSteps
+    {
+        [When(@"I click on button with ""(.*)""")]
+        public void WhenIClickOnButtonWith(string id)
+        {
+            Browser.Current.Button(By.Id(id)).Click();
+        }
+
+        [Then(@"there are buttons with id ""(.*)""")]
+        public void ThenThereAreButtonsWithId(string[] ids)
+        {
+            Browser.Current.Buttons().Select(x => x.Id).Should().BeEquivalentTo(ids.AsEnumerable());
+        }
+
+        [Then(@"I see an alert with text ""(.*)""")]
+        public void ThenISeeAnAlertWithText(string text)
+        {
+            var alert = Browser.Current.SwitchTo().Alert();
+            var actualText = alert.Text;
+            alert.Accept();
+            actualText.Should().Be(text);
+        }
+    }
+}
