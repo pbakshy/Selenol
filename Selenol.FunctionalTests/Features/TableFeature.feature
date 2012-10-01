c91a003b173c1e7abc3fc5d18ce4deb8aa4548b1
mezm <bartleby.sev@gmail.com>
2012-10-01 14:08:47 +0300

    SE-41 Create functional tests
    covered table element

---
+++ Selenol.FunctionalTests/Features/TableFeature.feature
@@ -0,0 +1,26 @@
+﻿Feature: TableFeature
+	As an user I want to have ability to work with strong typed table element.
+
+Scenario: All tables
+	Given that I am viewing "Elements" page
+	Then there are tables with id "table-1, table-2"
+
+Scenario: Browsing simple table
+	Given that I am viewing "Elements" page
+	When I looking at table with id "table-1"
+	Then the table has 2 rows
+	And each row has 2 cells
+	And 2nd cell in 1st row has text "table 1 row 1 cell 2"
+	And 1st cell in 2nd row has text "table 1 row 2 cell 1"
+
+Scenario: Browsing complex table
+	Given that I am viewing "Elements" page
+	When I looking at table with id "table-2"
+	Then the table has 5 rows
+	And the table has 2 rows in header
+	And the table has 2 rows in body
+	And the table has 1 row in footer
+	And 1st cell in 1st row has text "Head"
+	And 1st cell in 1st row in the table head has text "Head" 
+	And 1st cell in 2nd row has text "Subhead 1"
+	And 1st cell in 2nd row in the table body has text "table 2 row 2 cell 1" 
---
+++ Selenol.FunctionalTests/Steps/TableSteps.cs
@@ -0,0 +1,83 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+
+using FluentAssertions;
+
+using OpenQA.Selenium;
+
+using Selenol.Elements;
+
+using TechTalk.SpecFlow;
+
+namespace Selenol.FunctionalTests.Steps
+{
+    [Binding]
+    public class TableSteps
+    {
+        private TableElement table;
+
+        [When(@"I looking at table with id ""(.*)""")]
+        public void WhenIBrowsingTableWithId(string id)
+        {
+            this.table = Browser.Current.Table(By.Id(id));
+        }
+
+        [Then(@"there are tables with id ""(.*)""")]
+        public void ThenThereAreTablesWithId(string[] ids)
+        {
+            Browser.Current.Tables().Select(x => x.Id).Should().BeEquivalentTo(ids.AsEnumerable());
+        }
+
+        [Then(@"the table has (.*) rows?")]
+        public void ThenTheTableHasRows(int rowCount)
+        {
+            this.table.AllRows.Count().Should().Be(rowCount);
+        }
+
+        [Then(@"each row has (.*) cells")]
+        public void ThenEachRowHasCells(int cellCount)
+        {
+            foreach (var row in this.table.AllRows)
+            {
+                row.Cells.Count().Should().Be(cellCount);
+            }
+        }
+
+        [Then(@"(.*)\w{2} cell in (.*)\w{2} row has text ""(.*)""")]
+        public void ThenNdCellInStRowHasText(int cellIndex, int rowIndex, string text)
+        {
+            this.table.AllRows.ToArray()[rowIndex - 1].Cells.ToArray()[cellIndex - 1].Text.Should().Be(text);
+        }
+
+        [Then(@"(.*)\w{2} cell in (.*)\w{2} row in the table head has text ""(.*)""")]
+        public void ThenStCellInStRowHasTextInTheTableHead(int cellIndex, int rowIndex, string text)
+        {
+            this.table.HeadRows.ToArray()[rowIndex - 1].Cells.ToArray()[cellIndex - 1].Text.Should().Be(text);
+        }
+
+        [Then(@"(.*)\w{2} cell in (.*)\w{2} row in the table body has text ""(.*)""")]
+        public void ThenStCellInNdRowInTheTableBodyHasText(int cellIndex, int rowIndex, string text)
+        {
+            this.table.BodyRows.ToArray()[rowIndex - 1].Cells.ToArray()[cellIndex - 1].Text.Should().Be(text);
+        }
+
+        [Then(@"the table has (.*) rows? in header")]
+        public void ThenTheTableHasRowsInHeader(int rowCount)
+        {
+            this.table.HeadRows.Count().Should().Be(rowCount);
+        }
+
+        [Then(@"the table has (.*) rows? in body")]
+        public void ThenTheTableHasRowsInBody(int rowCount)
+        {
+            this.table.BodyRows.Count().Should().Be(rowCount);
+        }
+
+        [Then(@"the table has (.*) rows? in footer")]
+        public void ThenTheTableHasRowsInFooter(int rowCount)
+        {
+            this.table.FooterRows.Count().Should().Be(rowCount);
+        }
+    }
+}
