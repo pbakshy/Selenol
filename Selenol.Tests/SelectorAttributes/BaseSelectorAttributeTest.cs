7bbff67d1927394adbc8935b28e16601813a6649
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-11 21:00:54 +0200

    Extracted BaseSelectorAttributeTest from TestIdAttribute

---
+++ Selenol.Tests/SelectorAttributes/BaseSelectorAttributeTest.cs
@@ -0,0 +1,113 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Rhino.Mocks;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    public abstract class BaseSelectorAttributeTest<TValidPage, TDerivedValidPage>
+        where TValidPage : SimplePageForTest, new() where TDerivedValidPage : TValidPage, new()
+    {
+        protected const string TestSelector = "test-selector";
+
+        protected IWebDriver WebDriver { get; private set; }
+
+        protected IJavaScriptExecutor JavaScriptExecutor { get; private set; }
+
+        protected IWebElement WebElement { get; private set; }
+
+        [SetUp]
+        public void Init()
+        {
+            this.WebDriver = MockRepository.GenerateStub<IWebDriver>();
+            this.JavaScriptExecutor = MockRepository.GenerateStub<IJavaScriptExecutor>();
+            this.WebElement = MockRepository.GenerateStub<IWebElement>();
+
+            this.WebDriver.Url = "/myhome/page.aspx";
+        }
+
+        [Test]
+        public void CanBeUsedForAutoProperty()
+        {
+            var page = PageFactory.Create<TValidPage>(this.WebDriver, this.JavaScriptExecutor);
+            this.AssertCorrectSelectorAttributeUsage(page);
+        }
+
+        [Test]
+        public void ShouldProxyPropertyFromBaseClass()
+        {
+            var page = PageFactory.Create<TDerivedValidPage>(this.WebDriver, this.JavaScriptExecutor);
+            this.AssertCorrectSelectorAttributeUsage(page);
+        }
+
+        [Test]
+        public void IncorrectAttributeUsage()
+        {
+            var exception = Assert.Throws<PageInitializationException>(() => this.CreatePageWithIncorrectAttributeUsage());
+            exception.Message.Should()
+                     .Contain("'NotAuthoProperty' is not an auto property. Selector attributes can be used only for auto properties.")
+                     .And
+                     .Contain("'PropertyWithoutSetter' is not an auto property. Selector attributes can be used only for auto properties.")
+                     .And
+                     .Contain(
+                         "'NotElement' property has invalid type. Selector attributes can be used only for properties with type derived from BaseHtmlElement.")
+                     .And
+                     .Contain("'AbstractElement' property has invalid type. Selector attributes can not be used for abstract types.")
+                     .And
+                     .Contain("'NotVirtualProperty' is not virtual. Selector attributes can be used only for virtual properties.")
+                     .And
+                     .Contain("'PropertyWithoutGetter' is not an auto property. Selector attributes can be used only for auto properties.");
+
+            AssertSinglePropertyError(exception, "NotAuthoProperty");
+            AssertSinglePropertyError(exception, "NotVirtualProperty");
+            AssertSinglePropertyError(exception, "AbstractElement");
+            AssertSinglePropertyError(exception, "NotElement");
+        }
+
+        [Test]
+        public void ThrowsForNullSelector()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => this.CreatePageWithNullSelector());
+            exception.Message.Should().Contain("Parameter can not be null or empty.");
+        }
+
+        [Test]
+        public void ThrowsForEmptySelector()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => this.CreatePageWithEmptySelector());
+            exception.Message.Should().Contain("Parameter can not be null or empty.");
+        }
+
+        protected abstract ButtonElement GetButton(TValidPage page);
+
+        protected abstract BasePage CreatePageWithIncorrectAttributeUsage();
+        
+        protected abstract BasePage CreatePageWithNullSelector();
+
+        protected abstract BasePage CreatePageWithEmptySelector();
+
+        private static void AssertSinglePropertyError(Exception exception, string propertyName)
+        {
+            var count = new Regex(propertyName).Matches(exception.Message).Count;
+            count.Should().Be(1);
+        }
+
+        private void AssertCorrectSelectorAttributeUsage(TValidPage page)
+        {
+            this.WebElement.Stub(x => x.TagName).Return("button");
+            this.WebDriver.Stub(x => x.FindElement(By.Id(TestSelector))).Return(this.WebElement);
+            this.WebElement.Stub(x => x.Text).Return("abcd");
+
+            var button = this.GetButton(page);
+
+            button.Text.Should().Be("abcd");
+            button.Should().NotBeSameAs(this.GetButton(page));
+        }
+    }
+}
--- Selenol.Tests/SelectorAttributes/TestIdAttribute.cs
+++ Selenol.Tests/SelectorAttributes/TestIdAttribute.cs
@@ -1,112 +1,39 @@
 ﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
 
-using System;
-using System.Text.RegularExpressions;
-using FluentAssertions;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using Rhino.Mocks;
 using Selenol.Elements;
 using Selenol.Page;
 using Selenol.SelectorAttributes;
 
 namespace Selenol.Tests.SelectorAttributes
 {
-    /// <summary>The test id attribute.</summary>
     [TestFixture]
-    public class TestIdAttribute
+    public class TestIdAttribute : BaseSelectorAttributeTest<TestIdAttribute.PageWithSelectorAttribute,
+                                       TestIdAttribute.PageInheritsPropertiesWithSelectorAttribute>
     {
-        private IWebDriver webDriver;
-        private IJavaScriptExecutor javaScriptExecutor;
-        private IWebElement webElement;
-
-        [SetUp]
-        public void Init()
+        protected override ButtonElement GetButton(PageWithSelectorAttribute page)
         {
-            this.webDriver = MockRepository.GenerateStub<IWebDriver>();
-            this.javaScriptExecutor = MockRepository.GenerateStub<IJavaScriptExecutor>();
-            this.webElement = MockRepository.GenerateStub<IWebElement>();
-
-            this.webDriver.Url = "/myhome/page.aspx";
+            return page.Button;
         }
 
-        [Test]
-        public void CanBeUsedForAutoProperty()
+        protected override BasePage CreatePageWithIncorrectAttributeUsage()
         {
-            this.webElement.Stub(x => x.TagName).Return("button");
-            var page = PageFactory.Create<PageWithSelectorAttribute>(this.webDriver, this.javaScriptExecutor);
-            this.webDriver.Stub(x => x.FindElement(By.Id("test-selector"))).Return(this.webElement);
-            this.webElement.Stub(x => x.Text).Return("abcd");
-
-            var button = page.Button;
-
-            button.Text.Should().Be("abcd");
-            button.Should().NotBeSameAs(page.Button);
+            return PageFactory.Create<PageWithIncorrectSelectorAttributeUsage>(this.WebDriver, this.JavaScriptExecutor);
         }
 
-        [Test]
-        public void ShouldProxyPropertyFromBaseClass()
+        protected override BasePage CreatePageWithNullSelector()
         {
-            //todo refactor this with previous
-            this.webElement.Stub(x => x.TagName).Return("button");
-            var page = PageFactory.Create<PageInheritsPropertiesWithSelectorAttribute>(this.webDriver, this.javaScriptExecutor);
-            this.webDriver.Stub(x => x.FindElement(By.Id("test-selector"))).Return(this.webElement);
-            this.webElement.Stub(x => x.Text).Return("abcd");
-
-            var button = page.Button;
-
-            button.Text.Should().Be("abcd");
-            button.Should().NotBeSameAs(page.Button);
-        }
-        
-        [Test]
-        public void IncorrectAttributeUsage()
-        {
-            var exception =
-                Assert.Throws<Exception>(() => PageFactory.Create<PageWithIncorrectSelectorAttributeUsage>(this.webDriver, this.javaScriptExecutor));
-            exception.Message.Should()
-                     .Contain("'NotAuthoProperty' is not an auto property. Selector attributes can be used only for auto properties.")
-                     .And
-                     .Contain("'PropertyWithoutSetter' is not an auto property. Selector attributes can be used only for auto properties.")
-                     .And
-                     .Contain(
-                         "'NotElement' property has invalid type. Selector attributes can be used only for properties with type derived from BaseHtmlElement.")
-                     .And
-                     .Contain("'AbstractElement' property has invalid type. Selector attributes can not be used for abstract types.")
-                     .And
-                     .Contain("'NotVirtualProperty' is not virtual. Selector attributes can be used only for virtual properties.")
-                     .And
-                     .Contain("'PropertyWithoutGetter' is not an auto property. Selector attributes can be used only for auto properties.");
-
-            AssertSinglePropertyError(exception, "NotAuthoProperty");
-            AssertSinglePropertyError(exception, "NotVirtualProperty");
-            AssertSinglePropertyError(exception, "AbstractElement");
-            AssertSinglePropertyError(exception, "NotElement");
+            return PageFactory.Create<PageWithNullSelector>(this.WebDriver, this.JavaScriptExecutor);
         }
 
-        [Test]
-        public void ThrowsForNullSelector()
+        protected override BasePage CreatePageWithEmptySelector()
         {
-            var exception = Assert.Throws<ArgumentException>(() => PageFactory.Create<PageWithNullSelector>(this.webDriver, this.javaScriptExecutor));
-            exception.Message.Should().Contain("Parameter can not be null or empty.");
+            return PageFactory.Create<PageWithEmptySelector>(this.WebDriver, this.JavaScriptExecutor);
         }
 
-        [Test]
-        public void ThrowsForEmptySelector()
-        {
-            var exception = Assert.Throws<ArgumentException>(() => PageFactory.Create<PageWithEmptySelector>(this.webDriver, this.javaScriptExecutor));
-            exception.Message.Should().Contain("Parameter can not be null or empty.");
-        }
-
-        private static void AssertSinglePropertyError(Exception exception, string propertyName)
-        {
-            var count = new Regex(propertyName).Matches(exception.Message).Count;
-            count.Should().Be(1);
-        }
-        
         public class PageWithSelectorAttribute : SimplePageForTest
         {
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual ButtonElement Button { get; private set; }
         }
 
@@ -118,7 +45,7 @@ namespace Selenol.Tests.SelectorAttributes
         {
             private TextboxElement notAuthoProperty;
 
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual TextboxElement NotAuthoProperty
             {
                 get
@@ -132,8 +59,7 @@ namespace Selenol.Tests.SelectorAttributes
                 }
             }
 
-            /// <summary>Gets the property without setter.</summary>
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual TextAreaElement PropertyWithoutSetter
             {
                 get
@@ -142,16 +68,16 @@ namespace Selenol.Tests.SelectorAttributes
                 }
             }
 
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual int NotElement { get; set; }
 
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual BaseHtmlElement AbstractElement { get; set; }
 
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public SelectElement NotVirtualProperty { get; set; }
 
-            [Id("test-selector")]
+            [Id(TestSelector)]
             public virtual TextboxElement PropertyWithoutGetter
             {
                 set
