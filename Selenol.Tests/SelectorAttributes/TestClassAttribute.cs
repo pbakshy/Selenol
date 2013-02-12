f3649fc69c4518e0cc7a27fe1d90858bed73611f
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-12 19:37:41 +0200

    SE-44 Implement element selection by class

---
+++ Selenol.Tests/SelectorAttributes/TestClassAttribute.cs
@@ -0,0 +1,86 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.SelectorAttributes;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    [TestFixture]
+    public class TestClassAttribute : BaseSelectorAttributeTest
+    {
+        protected override By GetByCriteria(string selectorValue)
+        {
+            return By.ClassName(selectorValue);
+        }
+
+        public class PageWithSelectorAttribute : SimplePageForTest
+        {
+            [Class(TestSelector)]
+            public virtual ButtonElement Button { get; private set; }
+        }
+
+        public class PageInheritsPropertiesWithSelectorAttribute : PageWithSelectorAttribute
+        {
+        }
+
+        public class PageWithIncorrectSelectorAttributeUsage : SimplePageForTest
+        {
+            private TextboxElement notAuthoProperty;
+
+            [Class(TestSelector)]
+            public virtual TextboxElement NotAuthoProperty
+            {
+                get
+                {
+                    return this.notAuthoProperty;
+                }
+
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+
+            [Class(TestSelector)]
+            public virtual TextAreaElement PropertyWithoutSetter
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            [Class(TestSelector)]
+            public virtual int NotElement { get; set; }
+
+            [Class(TestSelector)]
+            public virtual BaseHtmlElement AbstractElement { get; set; }
+
+            [Class(TestSelector)]
+            public SelectElement NotVirtualProperty { get; set; }
+
+            [Class(TestSelector)]
+            public virtual TextboxElement PropertyWithoutGetter
+            {
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+        }
+
+        public class PageWithNullSelector : SimplePageForTest
+        {
+            [Class(null)]
+            public virtual TextboxElement TextboxElement { get; set; } 
+        }
+
+        public class PageWithEmptySelector : SimplePageForTest
+        {
+            [Class("")]
+            public virtual FormElement FormElement { get; set; }
+        }
+    }
+}
---
+++ Selenol/SelectorAttributes/ClassAttribute.cs
@@ -0,0 +1,31 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.SelectorAttributes
+{
+    /// <summary>
+    /// The Class selector attribute. Can be used for dynamic selection of elements by their class name. 
+    /// Element must derived from <see cref="BaseHtmlElement"/> and used as an auto-property of class derived from <see cref="BasePage"/>
+    /// </summary>
+    public class ClassAttribute : BaseSelectorAttribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="ClassAttribute"/> class.</summary>
+        /// <param name="className">The class Name.</param>
+        public ClassAttribute(string className)
+            : base(className)
+        {
+        }
+
+        /// <summary>Gets the <see cref="By.ClassName"/> selector.</summary>
+        public override By Selector
+        {
+            get
+            {
+                return By.ClassName(this.Value);
+            }
+        }
+    }
+}
