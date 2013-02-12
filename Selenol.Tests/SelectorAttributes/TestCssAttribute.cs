8a65e2e0b58262d1785e0b4adf9bf893ef36d83d
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-12 19:51:02 +0200

    SE-45 Implement element selection by css selector

---
+++ Selenol.Tests/SelectorAttributes/TestCssAttribute.cs
@@ -0,0 +1,86 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.SelectorAttributes;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    [TestFixture]
+    public class TestCssAttribute : BaseSelectorAttributeTest
+    {
+        protected override By GetByCriteria(string selectorValue)
+        {
+            return By.CssSelector(selectorValue);
+        }
+
+        public class PageWithSelectorAttribute : SimplePageForTest
+        {
+            [Css(TestSelector)]
+            public virtual ButtonElement Button { get; private set; }
+        }
+
+        public class PageInheritsPropertiesWithSelectorAttribute : PageWithSelectorAttribute
+        {
+        }
+
+        public class PageWithIncorrectSelectorAttributeUsage : SimplePageForTest
+        {
+            private TextboxElement notAuthoProperty;
+
+            [Css(TestSelector)]
+            public virtual TextboxElement NotAuthoProperty
+            {
+                get
+                {
+                    return this.notAuthoProperty;
+                }
+
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+
+            [Css(TestSelector)]
+            public virtual TextAreaElement PropertyWithoutSetter
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            [Css(TestSelector)]
+            public virtual int NotElement { get; set; }
+
+            [Css(TestSelector)]
+            public virtual BaseHtmlElement AbstractElement { get; set; }
+
+            [Css(TestSelector)]
+            public SelectElement NotVirtualProperty { get; set; }
+
+            [Css(TestSelector)]
+            public virtual TextboxElement PropertyWithoutGetter
+            {
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+        }
+
+        public class PageWithNullSelector : SimplePageForTest
+        {
+            [Css(null)]
+            public virtual TextboxElement TextboxElement { get; set; }
+        }
+
+        public class PageWithEmptySelector : SimplePageForTest
+        {
+            [Css("")]
+            public virtual FormElement FormElement { get; set; }
+        }
+    }
+}
---
+++ Selenol/SelectorAttributes/CssAttribute.cs
@@ -0,0 +1,31 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.SelectorAttributes
+{
+    /// <summary>
+    /// The CSS selector attribute. Can be used for dynamic selection of elements using CSS selectors. 
+    /// Element must derived from <see cref="BaseHtmlElement"/> and used as an auto-property of class derived from <see cref="BasePage"/>
+    /// </summary>
+    public class CssAttribute : BaseSelectorAttribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="CssAttribute"/> class.</summary>
+        /// <param name="cssSelector">The CSS selector.</param>
+        public CssAttribute(string cssSelector)
+            : base(cssSelector)
+        {
+        }
+
+        /// <summary>Gets the <see cref="By.CssSelector"/>.</summary>
+        public override By Selector
+        {
+            get
+            {
+                return By.CssSelector(this.Value);
+            }
+        }
+    }
+}
