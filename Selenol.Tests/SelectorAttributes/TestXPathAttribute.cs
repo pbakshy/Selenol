eb8de913a2291a95585f1b3cae80897d80754f03
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-12 20:13:26 +0200

    SE-46 Implement element selection by xpath

---
+++ Selenol.Tests/SelectorAttributes/TestXPathAttribute.cs
@@ -0,0 +1,86 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.SelectorAttributes;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    [TestFixture]
+    public class TestXPathAttribute : BaseSelectorAttributeTest
+    {
+        protected override By GetByCriteria(string selectorValue)
+        {
+            return By.XPath(selectorValue);
+        }
+
+        public class PageWithSelectorAttribute : SimplePageForTest
+        {
+            [XPath(TestSelector)]
+            public virtual ButtonElement Button { get; private set; }
+        }
+
+        public class PageInheritsPropertiesWithSelectorAttribute : PageWithSelectorAttribute
+        {
+        }
+
+        public class PageWithIncorrectSelectorAttributeUsage : SimplePageForTest
+        {
+            private TextboxElement notAuthoProperty;
+
+            [XPath(TestSelector)]
+            public virtual TextboxElement NotAuthoProperty
+            {
+                get
+                {
+                    return this.notAuthoProperty;
+                }
+
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+
+            [XPath(TestSelector)]
+            public virtual TextAreaElement PropertyWithoutSetter
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            [XPath(TestSelector)]
+            public virtual int NotElement { get; set; }
+
+            [XPath(TestSelector)]
+            public virtual BaseHtmlElement AbstractElement { get; set; }
+
+            [XPath(TestSelector)]
+            public SelectElement NotVirtualProperty { get; set; }
+
+            [XPath(TestSelector)]
+            public virtual TextboxElement PropertyWithoutGetter
+            {
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+        }
+
+        public class PageWithNullSelector : SimplePageForTest
+        {
+            [XPath(null)]
+            public virtual TextboxElement TextboxElement { get; set; }
+        }
+
+        public class PageWithEmptySelector : SimplePageForTest
+        {
+            [XPath("")]
+            public virtual FormElement FormElement { get; set; }
+        }
+    }
+}
---
+++ Selenol/SelectorAttributes/XPathAttribute.cs
@@ -0,0 +1,31 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.SelectorAttributes
+{
+    /// <summary>
+    /// The XPath selector attribute. Can be used for dynamic selection of elements using XPath. 
+    /// Element must be derived from <see cref="BaseHtmlElement"/> and used as an auto-property of class derived from <see cref="BasePage"/>
+    /// </summary>
+    public class XPathAttribute : BaseSelectorAttribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="XPathAttribute"/> class.</summary>
+        /// <param name="xpath">The CSS selector.</param>
+        public XPathAttribute(string xpath)
+            : base(xpath)
+        {
+        }
+
+        /// <summary>Gets the <see cref="By.XPath"/> selector.</summary>
+        public override By Selector
+        {
+            get
+            {
+                return By.XPath(this.Value);
+            }
+        }
+    }
+}
