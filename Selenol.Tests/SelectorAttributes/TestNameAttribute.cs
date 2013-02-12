1d6e3f6e472c9efbab9ecb465b557bb3ced427c9
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-12 20:21:03 +0200

    SE-52 Implement element selection by name attribute

---
+++ Selenol.Tests/SelectorAttributes/TestNameAttribute.cs
@@ -0,0 +1,84 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.SelectorAttributes;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    public class TestNameAttribute : BaseSelectorAttributeTest
+    {
+        protected override By GetByCriteria(string selectorValue)
+        {
+            return By.Name(selectorValue);
+        }
+
+        public class PageWithSelectorAttribute : SimplePageForTest
+        {
+            [Name(TestSelector)]
+            public virtual ButtonElement Button { get; private set; }
+        }
+
+        public class PageInheritsPropertiesWithSelectorAttribute : PageWithSelectorAttribute
+        {
+        }
+
+        public class PageWithIncorrectSelectorAttributeUsage : SimplePageForTest
+        {
+            private TextboxElement notAuthoProperty;
+
+            [Name(TestSelector)]
+            public virtual TextboxElement NotAuthoProperty
+            {
+                get
+                {
+                    return this.notAuthoProperty;
+                }
+
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+
+            [Name(TestSelector)]
+            public virtual TextAreaElement PropertyWithoutSetter
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            [Name(TestSelector)]
+            public virtual int NotElement { get; set; }
+
+            [Name(TestSelector)]
+            public virtual BaseHtmlElement AbstractElement { get; set; }
+
+            [Name(TestSelector)]
+            public SelectElement NotVirtualProperty { get; set; }
+
+            [Name(TestSelector)]
+            public virtual TextboxElement PropertyWithoutGetter
+            {
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+        }
+
+        public class PageWithNullSelector : SimplePageForTest
+        {
+            [Name(null)]
+            public virtual TextboxElement TextboxElement { get; set; }
+        }
+
+        public class PageWithEmptySelector : SimplePageForTest
+        {
+            [Name("")]
+            public virtual FormElement FormElement { get; set; }
+        }
+    }
+}
---
+++ Selenol/SelectorAttributes/NameAttribute.cs
@@ -0,0 +1,31 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.SelectorAttributes
+{
+    /// <summary>
+    /// The Name selector attribute. Can be used for dynamic selection of elements by their name. 
+    /// Element must be derived from <see cref="BaseHtmlElement"/> and used as an auto-property of class derived from <see cref="BasePage"/>
+    /// </summary>
+    public class NameAttribute : BaseSelectorAttribute
+    {
+        /// <summary>Initializes a new instance of the <see cref="NameAttribute"/> class.</summary>
+        /// <param name="name">The name attribute selector.</param>
+        public NameAttribute(string name)
+            : base(name)
+        {
+        }
+
+        /// <summary>Gets the <see cref="By.Name"/> selector.</summary>
+        public override By Selector
+        {
+            get
+            {
+                return By.Name(this.Value);
+            }
+        }
+    }
+}
