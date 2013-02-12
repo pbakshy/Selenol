d71f69c7b374e530c378255f26f02bdf6c82064b
Pavel Bakshy <pavelbakshy@gmail.com>
2013-02-12 20:35:10 +0200

    SE-53 Implement element selection by tag name

---
+++ Selenol.Tests/SelectorAttributes/TestTagNameAttribute.cs
@@ -0,0 +1,84 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.SelectorAttributes;
+
+namespace Selenol.Tests.SelectorAttributes
+{
+    public class TestTagNameAttribute : BaseSelectorAttributeTest
+    {
+        protected override By GetByCriteria(string selectorValue)
+        {
+            return By.TagName(selectorValue);
+        }
+
+        public class PageWithSelectorAttribute : SimplePageForTest
+        {
+            [TagName(TestSelector)]
+            public virtual ButtonElement Button { get; private set; }
+        }
+
+        public class PageInheritsPropertiesWithSelectorAttribute : PageWithSelectorAttribute
+        {
+        }
+
+        public class PageWithIncorrectSelectorAttributeUsage : SimplePageForTest
+        {
+            private TextboxElement notAuthoProperty;
+
+            [TagName(TestSelector)]
+            public virtual TextboxElement NotAuthoProperty
+            {
+                get
+                {
+                    return this.notAuthoProperty;
+                }
+
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+
+            [TagName(TestSelector)]
+            public virtual TextAreaElement PropertyWithoutSetter
+            {
+                get
+                {
+                    return null;
+                }
+            }
+
+            [TagName(TestSelector)]
+            public virtual int NotElement { get; set; }
+
+            [TagName(TestSelector)]
+            public virtual BaseHtmlElement AbstractElement { get; set; }
+
+            [TagName(TestSelector)]
+            public SelectElement NotVirtualProperty { get; set; }
+
+            [TagName(TestSelector)]
+            public virtual TextboxElement PropertyWithoutGetter
+            {
+                set
+                {
+                    this.notAuthoProperty = value;
+                }
+            }
+        }
+
+        public class PageWithNullSelector : SimplePageForTest
+        {
+            [TagName(null)]
+            public virtual TextboxElement TextboxElement { get; set; }
+        }
+
+        public class PageWithEmptySelector : SimplePageForTest
+        {
+            [TagName("")]
+            public virtual FormElement FormElement { get; set; }
+        }
+    }
+}
---
+++ Selenol/SelectorAttributes/TagNameAttribute.cs
@@ -0,0 +1,35 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+using Selenol.Page;
+
+namespace Selenol.SelectorAttributes
+{
+    /// <summary>
+    /// The Tag Name selector attribute. Can be used for dynamic selection of elements by the tag name. 
+    /// Element must be derived from <see cref="BaseHtmlElement"/> and used as an auto-property of class derived from <see cref="BasePage"/>
+    /// </summary>
+    public class TagNameAttribute : BaseSelectorAttribute
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TagNameAttribute" /> class.
+        /// </summary>
+        /// <param name="tagName">The tag name.</param>
+        public TagNameAttribute(string tagName)
+            : base(tagName)
+        {
+        }
+
+        /// <summary>
+        ///     Gets the <see cref="By.TagName" /> selector.
+        /// </summary>
+        public override By Selector
+        {
+            get
+            {
+                return By.TagName(this.Value);
+            }
+        }
+    }
+}
