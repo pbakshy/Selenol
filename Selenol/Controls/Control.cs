7a4605f59264b0da64512f87204479d7c7d08f5b
Pavel Bakshy <pavelbakshy@gmail.com>
2014-07-30 11:15:08 +0400

    SE-55 Create base class for control

---
+++ Selenol/Controls/Control.cs
@@ -0,0 +1,19 @@
+﻿// ﻿Copyright (c) Pavel Bakshy, Valeriy Ogiy. All rights reserved. See License.txt in the project root for license information.
+
+using OpenQA.Selenium;
+using Selenol.Elements;
+
+namespace Selenol.Controls
+{
+    /// <summary>The Control class represents logically structured and/or reusable group of page elements.</summary>
+    /// <typeparam name="T">The type of container element which is used as basis for control.</typeparam>
+    public abstract class Control<T> : ContainerElement where T : ContainerElement
+    {
+        /// <summary>Initializes a new instance of the <see cref="Control{T}" /> class.</summary>
+        /// <param name="webElement">The web element.</param>
+        protected Control(IWebElement webElement) // может передавать в качестве параметра ContainerElement соответствующий...
+            : base(webElement)
+        {
+        }
+    }
+}
